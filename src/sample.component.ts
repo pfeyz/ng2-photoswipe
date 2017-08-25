c8585c536504b9e393d32688e526aa6beb6c2406
debian <ar>
2017-08-25 13:43:25 -0400

    adding css in style urls

--- src/sample.component.ts
+++ src/sample.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sample-component',
-  template: `<h1>Sample Component Change</h1>`
+  template: `<h1>Sample Component Change</h1>`,
+    styleUrls: [
+        'photoswipe/dist/photoswipe.css',
+        'photoswipe/dist/default-skin/default-skin.css',
+        'sample.component.css'
+    ],
 })
 export class SampleComponent {
 
