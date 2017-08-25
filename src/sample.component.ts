58603d9045aafb5d347e7bb5b3ebf48fdcd29507
Paul Feitzinger <paul@pfeyz.com>
2017-08-25 13:55:56 -0400

    fix photoswipe css paths

--- src/sample.component.ts
+++ src/sample.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'sample-component',
   template: `<h1>Sample Component Change</h1>`,
     styleUrls: [
-        'photoswipe/dist/photoswipe.css',
-        'photoswipe/dist/default-skin/default-skin.css',
+        '../node_modules/photoswipe/dist/photoswipe.css',
+        '../node_modules/photoswipe/dist/default-skin/default-skin.css',
         'sample.component.css'
     ],
 })
